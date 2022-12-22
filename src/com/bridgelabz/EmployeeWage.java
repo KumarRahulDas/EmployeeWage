cacd5a86f6a3a27a2152fee602232d82323d3f6e
KumarRahulDas <dasrahulbxrepa7061503554@gmail.com>
2022-12-22 01:55:48 -0800

    Added Employee Attendance

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,5 +3,12 @@ package com.bridgelabz;
 public class EmployeeWage {
     public static void main(String[] args){
         System.out.println("Welcome To UC Problems ");
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is Abscent");
+
     }
 }
